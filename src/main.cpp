d7463a03215164ea5cb17d12534b3a15d67578e3
Jobbet789 <jobvdveen@gmail.com>
2022-04-05 15:02:17 +0200

    Added some cases

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 #include <Core.h>
 #include <Wire.h>
 #include <Serial_C.h>
-// #include <LSM9DS1.h>
+#include <LSM9DS1.h>
+#include <Ultrasonic.h>
 
 Motor m1 (1, 2);
 Motor m2 (22, 23);
@@ -13,19 +14,13 @@ Motor m3 (24, 25);
 
 Lsm9ds1 gyro (300, -8.58);
 
+Ultrasonic ultra_L (8, 9);
+Ultrasonic ultra_R (39, 38);
+
 Serial_C serial (false);
 
 void move(float degrees, int basespeed) {
-  float pi = 57.29577951; // 1: 240, 2: 120, 3: 0
-  // float speedM1 = (cos((120-degrees) * pi)*basespeed);
-  // Serial.print("motor 1: ");
-  // Serial.println(speedM1);
-  // float speedM2 = (cos((240-degrees) * pi)*basespeed);
-  // Serial.print("motor 2: ");
-  // Serial.println(speedM2);
-  // float speedM3 = (cos((0-degrees) * pi)*basespeed);
-  // Serial.print("motor 3: ");
-  // Serial.println(speedM3);
+  float pi = 57.29577951;
 
   float speedM1 = -(basespeed) * sin((degrees + 180) / pi);
   float speedM2 = -(basespeed) * sin((degrees + 60) / pi);
@@ -40,15 +35,24 @@ void setup() {
   Serial.begin(115200);
   Serial.println("start");
   serial.setup();
-  // gyro.setup();
-  // gyro.magCalibrate();
+  gyro.setup();
+  gyro.magCalibrate();
   m1.setup();
   m2.setup();
   m3.setup();
 }
 
+int check_deg(int deg) { // last checks for degrees 
+  if (deg == 90) {
+   if (ultra_L.read() > ultra_R.read()) {
+     deg *= -1;
+   }
+  }
+  return deg;
+}
+
 void loop() {
-  float deg = serial.receive().toFloat();
-  //Serial.println(gyro.magCalculate());
-  move(deg, 0);
+  int deg = serial.receive().toInt();
+  Serial.println(gyro.magCalculate());
+  move(deg, 100);
 }
--- test/main.cpp
+++ test/main.cpp
@@ -38,9 +38,40 @@ void loop () {
     if (value == 1 or value == 13) {
       value = 0;
     }
-    serial.send(String(value * 25.7142857143));
+    serial.send(String(get_motor_angle(value)));
 
     Serial.print(value);
     Serial.print("\n");
     }
+}
+
+/* 
+0: ball is infront (drive forawrds)
+1: ball is to the right (drivce to the right until 1)
+2: ball is to the right, but under (drive down, until case 2)
+3: ball is excactly behind the robot (drive to the left, or to the right depending on the ultrasone)
+4: ball is to the left (drive to the left until 1)
+5: ball is to the left, but under (drive down, until case 5)
+
+*/
+
+
+
+int get_motor_angle(int deg_ball) {
+  int motor_deg;
+
+  switch (deg_ball)
+  {
+  case -20 ... 20:
+    motor_deg = (deg_ball + 180);
+  case 21 ... 70:
+    motor_deg = 90;
+  case 71 ... 160:
+    motor_deg = 180;
+  case -160 ... -71:
+    motor_deg = 180; 
+  default: 
+    motor_deg = 90;
+  }
+  return motor_deg;
 }
